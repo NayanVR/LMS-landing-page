b9c618f8c652f0a1c926b357ee78cc624a579953
aditya <4118adi@gmail.com>
2023-05-19 07:51:39 +0530

    changes before 1pm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
 
   return (
     <div className="bg-white">
       {/* Hero Section */}
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate px-6 pt-8 lg:px-8">
         {/* Top gradient */}
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -20,26 +21,86 @@ function App() {
           />
         </div>
 
+        <div className='w-full h-14 flex justify-between'>
+            <img src='gsfcu.png' />
+            <div className='flex justify-center items-center gap-4'>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900  sm:text-4xl">
+              GUIDE
+              </h1>
+              <p className="text-lg leading-8 text-gray-800">
+              GSFCU's / GIDM's Unified Internet based eDucational Eco-System
+              </p>
+            </div>
+            <img src='gidm.png' />
+        </div>
+
+        {/* Why LMS */}
+        <div className="mx-auto max-w-4xl py-32 sm:py-32 lg:py-32">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Why LMS?
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-800">
+            This is a learning management system developed to create efficient and skill enriched courses.The Learning Management System(LMS) GUIDE(GSFCU/GIDM's Unified Internet based eDucational Ecosystem) developed by the team of students of GSFC University in collabration with Gujarat Institute of Disaster Management(GIDM), Gandhinagar using Open edX Platform. Here we have studio for the faculties wherein they can effortlessly create knowledge, skill enriched content. And then we have the learner’s view wherein one can gain amazing amount of knowledge and skills through the medium of the LMS.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="https://mobile.gsfcuniversity.in/app.apk"
+                className="rounded-md flex items-center bg-primary pl-5 pr-7 py-3 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-all"
+                target='_blank'
+              >
+                <img className='w-6' src='android.svg' /> &nbsp;
+                GSFCU App
+              </a>
+
+              <a
+                href="https://mobile.guidelms.in/app.apk"
+                className="rounded-md flex items-center bg-primary pl-5 pr-7 py-3 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-all"
+                target='_blank'
+              >
+                <img className='w-6' src='android.svg' /> &nbsp;
+                GIDM App
+              </a> 
+            </div>
+          </div>
+        </div>
+
+        <hr/>
+
         {/* Main content */}
         <div className="mx-auto max-w-4xl py-32 sm:py-32 lg:py-32">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Learning Management System
+            GUIDE
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <p className="mt-6 text-lg leading-8 text-gray-800">
+            GSFCU's / GIDM's Unified Internet based eDucational Eco-System
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-primary px-6 py-3 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-all"
+            <div className="mt-4 flex items-center justify-center gap-x-6">
+            <a
+                href="https://mobile.gsfcuniversity.in/app.apk"
+                className="rounded-md flex items-center bg-primary pl-6 pr-7 py-3 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-all"
+                target='_blank'
               >
-                Get started
+                <img className='w-6' src='web.svg' /> &nbsp;
+                GSFCU
               </a>
+
+              <a
+                href="https://mobile.gsfcuniversity.in/app.apk"
+                className="rounded-md flex items-center bg-primary pl-6 pr-7 py-3 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-all"
+                target='_blank'
+              >
+                <img className='w-6' src='web.svg' /> &nbsp;
+                GIDM
+              </a> 
             </div>
           </div>
         </div>
+        
+        <hr/>
+        
+
       </div>
 
       <hr />
@@ -48,28 +109,46 @@ function App() {
       <div>
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
           <div className="max-w-3xl mx-auto text-center">
-            <h2 className="text-3xl font-extrabold text-gray-900">Meet the team</h2>
+            <h2 className="text-3xl font-extrabold text-primary">Meet the team</h2>
             <p className="mt-4 text-lg text-gray-500">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-              molestiae. Numquam corrupti in laborum sed rerum et corporis.
+              The hardworking team behind LMS.
             </p>
 
             <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-8 md:gap-y-10">
+              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
                 <TeamMember
-                  name="Name Surname"
-                  designation="Some Weird Designation"
+                  name="Tirth Jadvani"
+                  designation="4th Year - CSE"
+                  image="tirth.jpeg"
+                />
+                <TeamMember
+                  name="Veera Patel"
+                  designation="4th Year - CSE"
+                  image="veera.jpeg"
+                />
+              </dl>
+            </div>
+            <div className="mt-10">
+              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-4 md:gap-x-8 md:gap-y-10">
+                <TeamMember
+                  name="Nayan Radadiya"
+                  designation="2nd Year - CSE"
                   image="PFP.png"
                 />
                 <TeamMember
-                  name="Name Surname"
-                  designation="Some Weird Designation"
-                  image="PFP.png"
+                  name="Aditya Patel"
+                  designation="2nd Year - CSE"
+                  image="aditya.jpeg"
                 />
                 <TeamMember
-                  name="Name Surname"
-                  designation="Some Weird Designation"
-                  image="PFP.png"
+                  name="Shubham Kumar Roy"
+                  designation="2nd Year - CSE"
+                  image="shubham.jpeg"
+                />
+                <TeamMember
+                  name="Dhruvin Purohit"
+                  designation="2nd Year - CSE"
+                  image="dhruvin.png"
                 />
               </dl>
             </div>
